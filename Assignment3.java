9e90564f09c9506c57bc3852f6d87a7a3514f936
pgsr91 <pgsr91@hotmail.com>
2017-05-27 02:10:02 +0530

    Assignment 1.3

---
+++ Assignment3.java
@@ -0,0 +1,16 @@
+package Session;
+
+public class Assignment3 
+{
+	public static void main(String arg[])
+	{
+		int a=3, b=7; //variables.
+		System.out.println("Values are "+a+" and "+b);
+		//Swapping of two numbers.
+		a=a+b;
+		b=a-b;
+		a=a-b;	
+				//Printing swapped values.
+		System.out.println("After swapping values are... "+a+" and "+b);	
+	}
+}
